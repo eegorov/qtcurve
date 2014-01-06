e618802b5f673215a97b4cc5ddb3df623954e170
Yichao Yu <yyc1992@gmail.com>
2014-01-06 14:08:52 +0800

    [common] clean up

--- qt4/style/qtcurve.cpp
+++ qt4/style/qtcurve.cpp
@@ -3110,8 +3110,6 @@ int Style::pixelMetric(PixelMetric metric, const QStyleOption *option, const QWi
             return opts.windowBorder;
         case QtC_CustomBgnd:
             return qtcIsCustomBgnd(&opts);
-        case QtC_TitleBarButtonAppearance:
-            return (int)opts.titlebarButtonAppearance;
         case QtC_TitleAlignment:
             switch(opts.titlebarAlignment)
             {
